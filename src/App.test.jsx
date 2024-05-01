31733f2f608da439a0e8265dc736b6bd0e7485fc
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-01 18:59:43 +0200

    Delete sample content

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import App from './App.jsx'
-
-test('render hello world', () => {
-  render(<App />)
-
-  expect(screen.getByText(/hello react/i)).toBeInTheDocument()
-})
