6d8dba0670aa077c7b156b544043496893ddebc9
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-01 21:40:30 +0200

    Second test passes

--- src/components/RestaurantList.test.jsx
+++ src/components/RestaurantList.test.jsx
@@ -1,12 +1,32 @@
 import {vi} from 'vitest'
 import RestaurantList from './RestaurantList.jsx'
-import {render} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 
 describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
+  it('loa' + '' + 'ds restaurants on first render', () => {
     const loadRestaurants = vi.fn().mockName('loadRestaurants')
+    const restaurants = []
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />)
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    )
     expect(loadRestaurants).toHaveBeenCalled()
+  })
+
+  it('displays the restaurants', () => {
+    const noop = () => {}
+
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ]
+
+    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />)
+
+    expect(screen.getByText('Sushi Place')).toBeInTheDocument()
+    expect(screen.getByText('Pizza Place')).toBeInTheDocument()
   })
 })
