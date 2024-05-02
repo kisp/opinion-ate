770eca4d1ce3e02c0cc4f43a06e58b47819864f3
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-02 10:23:27 +0200

    Testing the store

---
+++ src/store/restaurants.test.js
@@ -0,0 +1,31 @@
+import {applyMiddleware, createStore} from 'redux'
+import {thunk, withExtraArgument} from 'redux-thunk'
+import restaurantsReducer from './restaurants/reducers.js'
+import {loadRestaurants} from './restaurants/actions.js'
+
+describe('restaurants', () => {
+  describe('loadRestaurants action', () => {
+    it('stores the restaurants', async () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ]
+
+      const api = {
+        loadRestaurants: () => Promise.resolve(records),
+      }
+
+      const initialState = {records: []}
+
+      const store = createStore(
+        restaurantsReducer,
+        initialState,
+        applyMiddleware(withExtraArgument(api)),
+      )
+
+      await store.dispatch(loadRestaurants())
+
+      expect(store.getState().records).toEqual(records)
+    })
+  })
+})
